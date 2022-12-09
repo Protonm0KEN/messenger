970f73fe4676f5e8ed66163f287e72b080f353b6
macbook <gamerovichakbar@gmail.com>
2022-12-09 21:17:22 +0500

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import MessengerFooter from './components/MessengerFooter/MessengerFooter';
+import MessengerHeader from './components/MessengerHeader/MessengerHeader';
+import MessengerMain from './components/MessengerMain/MessengerMain';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App__container container'>
+        <div className='messenger'>
+          <div className='messenger__wrapper'>
+            <MessengerHeader />
+            <MessengerMain/>
+            <MessengerFooter/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/MessengerFooter/MessengerFooter.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './MessengerFooter.scss'
+import camera from '../../assets/icons/Camera.svg'
+const MessengerFooter = () => {
+  return (
+    <footer className="footer">
+        <div className='footer__content'>
+            <input placeholder='Написать сообщение...' className='footer__content-input'></input>
+            <button className='footer__content-button'>
+                <img alt='camera' className='footer__content__button-img' src={camera}></img>
+            </button>
+        </div>
+    </footer>
+  )
+}
+
+export default MessengerFooter
